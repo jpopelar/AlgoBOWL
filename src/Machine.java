cdd92d7923d64b7e190a8e2d227e330c60d276ba
Guanhua Li <gli@mymail.mines.edu>
2017-02-20 13:46:24 -0700

    Added machine and task class

---
+++ src/Machine.java
@@ -0,0 +1,15 @@
+
+public class Machine {
+	public int index;
+	public int speed;
+	
+	public Machine(int index, int speed){
+		this.index = index;
+		this.speed = speed;
+	}
+	
+	public Machine(){
+		index = 0;
+		speed = 0;
+	}
+}
---
+++ src/Task.java
@@ -0,0 +1,15 @@
+
+public class Task {
+	public int index;
+	public int runtime;
+	
+	public Task(int index, int runtime){
+		this.index = index;
+		this.runtime = runtime;
+	}
+	
+	public Task(){
+		index = 0;
+		runtime = 0;
+	}
+}
